7a5ac8da1479d4e6f94be7621f29ab2b7fd134a7
VISHAKHA SAINI <vishakhasaini7906@gmail.com>
2022-02-18 19:20:02 +0530

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,10 @@
+class Matrix:
+    def __init__(self, matrix_string): 
+#def __int__ allows us to assign the class through instance x = Matrix(some_string)
+            self.matrix = [[int(i) for i in e.split()] for e in matrix_string.split("\n")) 
+#single line nested for loop
+ 
+    def row(self, index):
+        return self.matrix[index - 1]
+    def column(self, index):
+        return [r[index - 1] for r in self.matrix]
