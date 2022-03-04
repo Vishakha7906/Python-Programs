7cbd35c3a9307ebe07d0f95abe07e38cadda4a43
VISHAKHA SAINI <vishakhasaini7906@gmail.com>
2022-03-04 21:15:26 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,2 +1,7 @@
+""" statement-: Reverse a string
+For example: input: "cool" output: "looc"
+https://exercism.org/tracks/python/exercises/reverse-string/edit
+"""
 def reverse(input=''):
-    return input [::-1]
+return input [::-1]
+    
